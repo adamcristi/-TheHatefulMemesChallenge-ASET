030c55ff9e7a9518a08b6dce4ca5ac7cbecb930b
Chiparus Denis <34250243+KypY12@users.noreply.github.com>
2020-11-24 15:45:44 +0200

    add simple preprocess for images and refactoring to make it modular

--- Implementation/classifiers/classifier.py
+++ Implementation/classifiers/classifier.py
@@ -1,16 +1,23 @@
+import abc
+
+
 class Classifier:
 
     def __init__(self):
         pass
 
+    @abc.abstractmethod
     def load_data(self, args):
         pass
 
+    @abc.abstractmethod
     def preprocess(self, args):
         pass
 
+    @abc.abstractmethod
     def train(self, args):
         pass
 
+    @abc.abstractmethod
     def show_best_result(self, args) -> float:
         pass
