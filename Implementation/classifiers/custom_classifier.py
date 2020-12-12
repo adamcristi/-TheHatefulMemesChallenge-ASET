ba76a6d26b5ab91b428582c9177502b55410768a
Chiparus Denis <34250243+KypY12@users.noreply.github.com>
2020-12-12 12:32:59 +0200

    logging

--- Implementation/classifiers/custom_classifier.py
+++ Implementation/classifiers/custom_classifier.py
@@ -1,3 +1,5 @@
+import time
+
 import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
@@ -5,6 +7,7 @@ from tqdm import tqdm
 from transformers import BertModel
 
 from Implementation.classifiers.classifier import *
+from Implementation.logging.logger import Logger
 from Implementation.preprocess.images.img2vecencoding import Img2VecEncoding
 from Implementation.preprocess.texts.bert_preprocessor import BertPreprocessor
 
@@ -15,18 +18,38 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class LocalNeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size):
         super(LocalNeuralNet, self).__init__()
 
-        self.linear_layer_1 = nn.Linear(input_size, hidden_size)
-        self.f1 = nn.Sigmoid()
+        first_layer = 256
+        second_layer = 128
 
-        self.linear_last = nn.Linear(hidden_size, 2)
+        # first hidden layer
+        self.linear_layer_1 = nn.Linear(input_size, first_layer)
+        self.activation_1 = nn.ReLU()
+        self.batchnorm_1 = nn.BatchNorm1d(first_layer, affine=False)
+        self.dropout_1 = nn.Dropout(p=0.2)
+
+        # second hidden layer
+        self.linear_layer_2 = nn.Linear(first_layer, second_layer)
+        self.activation_2 = nn.ReLU()
+        self.batchnorm_2 = nn.BatchNorm1d(second_layer, affine=False)
+        self.dropout_2 = nn.Dropout(p=0.4)
+
+        # last layer
+        self.linear_last = nn.Linear(second_layer, 2)
         # we use cross-entropy and it applies softmax automatically, so we don't need softmax here
 
     def forward(self, input_):
         output = self.linear_layer_1(input_)
-        output = self.f1(output)
+        output = self.activation_1(output)
+        output = self.batchnorm_1(output)
+        output = self.dropout_1(output)
+
+        output = self.linear_layer_2(output)
+        output = self.activation_2(output)
+        output = self.batchnorm_2(output)
+        output = self.dropout_2(output)
 
         output = self.linear_last(output)
 
@@ -63,10 +86,12 @@ class HatefulMemesDataset(Dataset):
 
 class CustomClassifier(Classifier):
 
-    def __init__(self):
+    def __init__(self, log_path):
         super().__init__()
 
         self.SAVE_PATH = ""
+
+        self.logger = Logger(log_path=log_path)
 
         self.bertModel = BertModel.from_pretrained(
             "bert-base-uncased",
@@ -76,17 +101,23 @@ class CustomClassifier(Classifier):
         ).requires_grad_(False).to(DEVICE)
 
         self.best_accuracy = 0
-        self.data = dict()
+        self.data = {"train": dict(), "valid": dict()}
+        # self.data = {"train": dict(), "valid": dict(), "test": dict()}
 
         self.model = []
 
-        self.dataset_obj = HatefulMemesDataset(self.data, self.bertModel)
+        self.dataset_objs = {
+            "train": HatefulMemesDataset(self.data["train"], self.bertModel),
+            "valid": HatefulMemesDataset(self.data["valid"], self.bertModel),
+            # "test": HatefulMemesDataset(self.data["test"], self.bertModel)
+        }
 
         self.batch_size = 1
         self.learning_rate = 0.1
 
     def load_data(self, args):
-        data_file = args
+        # dataset_usecase can be train, valid or test
+        data_file, data_usecase = args
         temp_data = []
 
         try:
@@ -97,48 +128,57 @@ class CustomClassifier(Classifier):
         except Exception as e:
             raise
 
-        self.data = dict({"id": [], "img": [], "label": [], "text": []})
-        for elem in temp_data:
-            self.data["id"] += [torch.tensor(int(elem["id"]))]
-            self.data["img"] += [elem["img"]]
-            self.data["label"] += [torch.tensor(int(elem["label"]))]
-            self.data["text"] += [elem["text"]]
+        self.data[data_usecase] = dict({"id": [], "img": [], "label": [], "text": []})
 
-        self.data["id"] = torch.tensor(self.data["id"])
-        self.data["label"] = torch.tensor(self.data["label"])
+        for elem in temp_data:
+            self.data[data_usecase]["id"] += [torch.tensor(int(elem["id"]))]
+            self.data[data_usecase]["img"] += [elem["img"]]
+            self.data[data_usecase]["label"] += [torch.tensor(int(elem["label"]))]
+            self.data[data_usecase]["text"] += [elem["text"]]
+
+        self.data[data_usecase]["id"] = torch.tensor(self.data[data_usecase]["id"])
+        self.data[data_usecase]["label"] = torch.tensor(self.data[data_usecase]["label"])
 
     def preprocess(self, args):
 
         # args example : (BertPreprocessor(pretrained_model_type='bert-base-uncased', do_lower_case=True),
         #                        Img2VecEncoding(precalculated=True, enc_path=ENCODED_IMG_PATH))
 
-        if type(self.data) is not dict or \
-                not all(key in self.data for key in ["id", "label", "text"]) or \
-                not all(len(self.data["id"]) == len_other
-                        for len_other in [len(self.data["label"]), len(self.data["text"])]):
-            raise Exception("Cannot preprocess data with that format!")
+        for key, value in self.data.items():
+
+            if type(value) is not dict or \
+                    not all(key in value for key in ["id", "label", "text"]) or \
+                    not all(len(value["id"]) == len_other
+                            for len_other in [len(value["label"]), len(value["text"])]):
+                raise Exception("Cannot preprocess \'" + key + "\' data with that format!")
 
         text_preprocessor, image_preprocessor = args
 
-        text_preprocessor.execute(self.data)
+        for key, value in self.data.items():
 
-        image_preprocessor.execute(self.data)
+            value["type"] = key
 
-        self.dataset_obj = HatefulMemesDataset(self.data, self.bertModel)
+            text_preprocessor.execute(value)
+
+            image_preprocessor.execute(value)
+
+            self.dataset_objs[key] = HatefulMemesDataset(value, self.bertModel)
 
     def train(self, args):
 
         self.batch_size, epochs, self.learning_rate, self.SAVE_PATH = args
 
-        iterations = np.math.ceil(len(self.dataset_obj) / self.batch_size)
+        train_dataset = self.dataset_objs["train"]
 
-        dataloader = DataLoader(dataset=self.dataset_obj, batch_size=self.batch_size, shuffle=True)
+        iterations = np.math.ceil(len(train_dataset) / self.batch_size)
+
+        dataloader = DataLoader(dataset=train_dataset, batch_size=self.batch_size, shuffle=True)
 
         # the size of the input (bertModel+imgEncoded)
-        input_size = next(iter(self.dataset_obj))[0].shape[0]
+        input_size = next(iter(train_dataset))[0].shape[0]
 
         # the model to train
-        self.model = LocalNeuralNet(input_size, 256).requires_grad_(True).to(DEVICE)
+        self.model = LocalNeuralNet(input_size).requires_grad_(True).to(DEVICE)
 
         # Enables training mode
         self.model.train()
@@ -147,12 +187,12 @@ class CustomClassifier(Classifier):
         crit = nn.CrossEntropyLoss()
 
         # optimizer
-        optim = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        optim = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate, weight_decay=0.00001)
 
         # train loop
         with torch.enable_grad():
-            for epoch in tqdm(range(0, epochs)):
-                for batch_index, (inputs, labels) in enumerate(dataloader):
+            for epoch in tqdm(range(0, epochs), position=0):
+                for batch_index, (inputs, labels) in tqdm(enumerate(dataloader), position=1):
                     dev_inputs = inputs.to(DEVICE)
                     dev_labels = labels.to(DEVICE)
 
@@ -166,14 +206,26 @@ class CustomClassifier(Classifier):
                     loss.backward()
                     optim.step()
 
-                    # if (batch_index + 1) % 10 == 0:
-                    #     print(f"Epoch {epoch + 1}/{epochs}, Step {batch_index + 1}/{iterations}, inputs {inputs.shape}")
+                train_accuracy, train_loss = self.test_model(data_used="train")
+                valid_accuracy, valid_loss = self.test_model(data_used="valid")
 
-                print(f"Epoch loss: {loss.item()}")
-                self.test_model()
+                if valid_accuracy > self.best_accuracy:
+                    self.best_accuracy = valid_accuracy
 
-        if self.SAVE_PATH != "":
-            self.save_model()
+                print(f"Epoch {epoch + 1}/{epochs}, train_loss: {train_loss}, train_accuracy: {train_accuracy}, " +
+                      f"valid_loss: {valid_loss}, valid_accuracy: {valid_accuracy}")
+
+                self.logger.log({"epoch": str(epoch + 1) + "/" + str(epochs),
+                                 "train_loss": train_loss,
+                                 "train_accuracy": train_accuracy,
+                                 "valid_loss": valid_loss,
+                                 "valid_accuracy": valid_accuracy})
+
+                self.save_model(self.SAVE_PATH + str(time.time()) + "_epoch_" + str(epoch + 1) + "_" + str(epochs),
+                                ".pth")
+
+        # if self.SAVE_PATH != "":
+        #     self.save_model(self.SAVE_PATH + str(time.time()) + "_final", ".pth")
 
     def compute_accuracy(self, outputs, labels):
 
@@ -192,14 +244,17 @@ class CustomClassifier(Classifier):
 
         return hits
 
-    def test_model(self):
+    def test_model(self, data_used="test"):
 
-        dataloader = DataLoader(dataset=self.dataset_obj, batch_size=self.batch_size, shuffle=True)
+        dataloader = DataLoader(dataset=self.dataset_objs[data_used], batch_size=self.batch_size, shuffle=True)
 
         accuracy = 0
 
         # Enables eval mode
         self.model.eval()
+
+        loss = 0
+        crit = nn.CrossEntropyLoss()
 
         # Don't update the gradients
         with torch.no_grad():
@@ -210,18 +265,20 @@ class CustomClassifier(Classifier):
                 # forward
                 outputs = self.model(dev_inputs)
 
+                loss += crit(outputs, dev_labels).item()
+
                 accuracy += self.compute_accuracy(outputs, dev_labels)
 
         # Go back to training mode
         self.model.train()
 
-        accuracy /= len(self.data["label"])
+        accuracy /= len(self.data[data_used]["label"])
+        loss /= len(self.data[data_used]["label"])
 
-        print(accuracy)
-        return accuracy
+        return accuracy, loss
 
-    def save_model(self):
-        torch.save(self.model, self.SAVE_PATH)
+    def save_model(self, path, ext):
+        torch.save(self.model, path+ext)
 
     def load_model(self, load_path):
         self.model = torch.load(load_path)
--- Implementation/run.py
+++ Implementation/run.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime
+
 from Implementation.classifiers.custom_classifier import CustomClassifier
 from Implementation.preprocess.images.img2vecencoding import Img2VecEncoding
 from Implementation.preprocess.texts.bert_preprocessor import BertPreprocessor
@@ -5,7 +8,11 @@ from Implementation.preprocess.texts.bert_preprocessor import BertPreprocessor
 # You need to create a txt file in which you add your path to the json file that contains the text of the memes
 # (that "train.jsonl") and then add the .txt file to .gitignore
 with open("meme_text_path.txt", "r") as path_file:
-    PATH = path_file.readline()
+    # .rstrip() -> without new lines at the end
+    TRAIN_PATH = path_file.readline().rstrip()
+    VALID_PATH = path_file.readline().rstrip()
+    TEST_PATH = path_file.readline().rstrip()
+
 
 # You also need to have the images in the path ".../Implementation/img/"
 # (but don't put them on github -> add them to .gitignore),
@@ -13,21 +20,25 @@ with open("meme_text_path.txt", "r") as path_file:
 
 
 # Path to the encoding of the images (this example is just for the first 10 memes in the "train.jsonl" file)
-ENCODED_IMG_PATH = "./encoded_images/encoded_10.pickle"
+ENCODED_IMG_PATH = "./encoded_images/encoded_full"
+ENCODED_IMG_EXT = ".pickle"
 
+classifier = CustomClassifier(log_path=r".\logging\results")
 
-classifier = CustomClassifier()
-
-classifier.load_data(PATH)
+classifier.load_data((TRAIN_PATH, "train"))
+classifier.load_data((VALID_PATH, "valid"))
+# Test set doesn't have labels for some reason ...
+# classifier.load_data((TEST_PATH, "test"))
 
 
 # "do_lower_case" should be True for uncased-bert and False for cased-bert.
 # "precalculated" means that the images were already encoded and saved in ENCODED_IMG_PATH.
 # If it is False, then it will recalculate the encodings and save them to ENCODED_IMG_PATH
 classifier.preprocess((BertPreprocessor(pretrained_model_type='bert-base-uncased', do_lower_case=True),
-                       Img2VecEncoding(precalculated=True, enc_path=ENCODED_IMG_PATH)))
+                       Img2VecEncoding(precalculated=True, enc_path=ENCODED_IMG_PATH, ext=ENCODED_IMG_EXT)))
 
 # The .pth extension is specific to pytorch data serialization
 # classifier.load_model("./saved_models/test_model.pth")
-classifier.train((32, 1, 0.001, "./saved_models/test_model.pth"))
-classifier.test_model()
+classifier.train((32, 20000, 0.001, "./saved_models/" + str(time.time()) + "_" +
+                  datetime.now().strftime("%m_%d_%Y_%H_%M_%S") + "_"))
+# classifier.test_model()
